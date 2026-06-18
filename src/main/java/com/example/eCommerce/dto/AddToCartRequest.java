dbf2502349203de31d9da90c40452dfc6a7a670b
Sneha Raj <sneharaj7645653@gmail.com>
2026-06-18 13:33:29 +0530

    Change AddToCartRequest class to private

--- src/main/java/com/example/eCommerce/dto/AddToCartRequest.java
+++ src/main/java/com/example/eCommerce/dto/AddToCartRequest.java
@@ -5,7 +5,7 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class AddToCartRequest {
+private class AddToCartRequest {
     private String userId;
     private String productId;
     private int quantity;
